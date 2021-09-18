85462b8298e7ac88a043af54ce13b9056818b3e3
Pogrebnyak-m <70861792+Pogrebnyak-m@users.noreply.github.com>
2021-09-19 00:05:23 +0300

    Update and rename Main.java to 3. Main.java

---
+++ LR_1/Tasks/3. Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x1 = Double.parseDouble("9.8");
+        double x2 = Double.parseDouble("3");
+        System.out.print("Введите a: ");
+        double a = new Scanner(System.in).nextDouble();
+        if ((a <= x1) && (a >= x2)) System.out.println("число принадлежит отрезку");
+        else System.out.println("число не принадлежит отрезку");
+}
